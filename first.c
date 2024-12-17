98178f52368befac256c288c9fe69a6ff677c0ea
mdalinesco1464 <mdalinesco1464@gmail.com>
2024-12-17 11:52:41 +0600

    add: new first.c file for showing git_vim collaboration

---
+++ first.c
@@ -0,0 +1,20 @@
+/*
+    Author: MD. Younus Ali
+    Date: 2024-12-17
+    Description: simple c code for git_vim collabration
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief Brief description of the main function
+ * 
+ * @return int - Describe the return value
+ */
+int main() {
+    // TODO: Implement functionality
+    printf("Hello, World!\n");
+
+    return 0;
+}
